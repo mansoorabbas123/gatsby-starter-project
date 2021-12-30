d8d08251d3ad76405bc3c6ee2b3aae0f03d6b773
mansoor abbas <mansoor.abbas651@gmail.com>
2021-12-30 19:28:54 +0500

    add contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby-practice-for-quiz",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId,
+        accessToken
+      }
+    }
+  ],
 };
